b4c34df4caabab1162db55919ccb4689184f2b68
Beeman-P <beemanmct2023@gamil.com>
2024-04-01 10:56:31 +0530

    changes

--- validate.c
+++ validate.c
@@ -5,7 +5,7 @@
 #include "lex.h"
 #include "lexical.h"
 
-Status read_and_validate(char *argv[],strinfo *info)
+Status read_and_validate(char *argv[],strinfo *info)                                       //check the given file is c or not
 {
 	if(strstr(argv[1],".c")!=NULL )
 	{
@@ -19,7 +19,7 @@ Status read_and_validate(char *argv[],strinfo *info)
 	return l_true;
 }
 
-Status open_file(strinfo *info)
+Status open_file(strinfo *info)                                                          //open the file
 {
 	info->str_edit = fopen(info->src_file,"r");
 	if(info->str_edit != NULL)
@@ -27,7 +27,7 @@ Status open_file(strinfo *info)
 	else
 		return l_false;
 }
-Status check_operator(char *opt)
+Status check_operator(char *opt)                                                        //check the operator using array of string
 {
 
 	char oper[60][10]={"->","++","--","++","--",">>","<<","<<=",">>=",
@@ -44,7 +44,7 @@ Status check_operator(char *opt)
 	}
 	return l_false;
 }
-Status check_keyword(char *str)
+Status check_keyword(char *str)                                                 //check the keyword using const char or we use array of string
 {
 	  const char* keywords[]  = { "auto",     "break",    "case",     "char", 
                                       "const",    "continue", "default",  "do", 
@@ -61,7 +61,7 @@ Status check_keyword(char *str)
     } 
     return l_false; 
 }
-Status lexcial_analyser(strinfo *info)
+Status lexcial_analyser(strinfo *info)                             //function to get a character from file
 {
 	char st;
 	int i=0,j=0,k=0,l=0,m=0;
@@ -71,27 +71,27 @@ Status lexcial_analyser(strinfo *info)
 	char con[20];
 	char hd[20];
 	char lt[50];
-	while((st=fgetc(info->str_edit))!=-1)
+	while((st=fgetc(info->str_edit))!=-1)                                     //get a character from file using fgetc to EOF
 	{
-		if(isalpha(st))
+		if(isalpha(st))                                                  //if char is atoz
 		  {
-			  str[i++]=st;
-			  while(isalpha(st=fgetc(info->str_edit)))
+			  str[i++]=st;                                           //store first letter in string      
+			  while(isalpha(st=fgetc(info->str_edit)))               //run a loop to get a characters
 			  {
-				        str[i++]=st;
+				        str[i++]=st;                             // store in string
 			  }
 			  str[i]='\0';
 			  if(strcmp(str,"sizeof")==0)
 			  {
-				  printf("Operator      :    %s\n",str);
+				  printf("Operator      :    %s\n",str);         //check sizeof
 			  }
 			  else
 			  {
-				  if(check_keyword(str)==l_true)
+				  if(check_keyword(str)==l_true)                //check the keyword
 				  {
 					  printf("Keyword       :    %s\n",str);
 				  }
-				  else if(st=='(' || st=='_') 
+				  else if(st=='(' || st=='_')                   //if character start from (,),_
 				  {
 					  printf("Indetifier    :    %s\n",str);
 				  }
@@ -101,11 +101,11 @@ Status lexcial_analyser(strinfo *info)
 				  }
 			  }
 			  i=0;
-			  fseek(info->str_edit, -1, SEEK_CUR);
+			  fseek(info->str_edit, -1, SEEK_CUR);                       //now st=( so go one cursor back to check the char
 		  }
-	      else if(ispunct(st))
+	      else if(ispunct(st))                                                  //check not a char not a digit
 		  {
-		         if (st=='#')
+		         if (st=='#')                                               //char start with # is a preprosser
 		           {
 			  hd[l++]=st;
 
@@ -118,26 +118,26 @@ Status lexcial_analyser(strinfo *info)
 			  fseek(info->str_edit,-1,SEEK_CUR);
 			  printf("Preprocess    :    %s\n",hd);
 	                   }
-			 else if (st==')'|| st == '(')
+			 else if (st==')'|| st == '(')                             //else strat with " it is a literal ex.inside a printf
 			 {
 				 printf("Operator      :     %c\n",st);
 				 if((st=getc(info->str_edit))=='"')
 				 {
-					 lt[m++]=st;
+					 lt[m++]=st;                                 
 					 while((st=getc(info->str_edit))!='"')
 					 {
 						 lt[m++]=st;
 					 }
-					 lt[m++]='"',lt[m]='\0';
+					 lt[m++]='"',lt[m]='\0';                   //store " to " in lt string
 					 m=0;
-			                 fseek(info->str_edit,1,SEEK_CUR);
+			                 fseek(info->str_edit,1,SEEK_CUR);         //now st=" one step back
 					 printf("Literal       :    %s\n",lt);
 				 }
-			         fseek(info->str_edit,-1,SEEK_CUR);
+			         fseek(info->str_edit,-1,SEEK_CUR);                //if fget not equal to " one step back
 			 }
-			  else
+			  else                                                    
 			  {
-			  n[j++]=st;
+			  n[j++]=st;                                              //check operation
 
 			  while(ispunct(st=fgetc(info->str_edit)))
 			  {
@@ -149,7 +149,7 @@ Status lexcial_analyser(strinfo *info)
 			  fseek(info->str_edit, -1, SEEK_CUR);
 			  }
 		  }
-	      else if(isdigit(st))
+	      else if(isdigit(st))                                               //check digit
 		  {
 			  con[k++]=st;
 			  while(isdigit(st=fgetc(info->str_edit)))
@@ -165,7 +165,7 @@ Status lexcial_analyser(strinfo *info)
 	return l_true;
 
 }
-Status start_process(strinfo *info)
+Status start_process(strinfo *info)                                             //start process 
 {
 	printf("Parsing    :    %s     :    Started \n",info->src_file);
 	if(lexcial_analyser(info)==l_true)
